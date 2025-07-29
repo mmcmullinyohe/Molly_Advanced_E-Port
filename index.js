c753674fe4134e8875c9792b7f848611632c1232
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-07-29 10:00:10 -0500

    background flowers

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-//template_z8antpa
-//service_2wrwhwn
-//Bh8Ce6IH2ELec4_B7
-
 let isModalOpen = false;
 let contrastToggle = false;
 const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    // Added rotate after tutorial
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+  }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+         document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
   event.preventDefault();
